95339fa971f5b44253108644779e903ae4b4545d
Diego Di Salvatore <disalvatorediego@gmail.com>
2018-01-23 18:23:37 -0400

    Fix public/index.php

--- public/index.php
+++ public/index.php
@@ -3,13 +3,12 @@
 	 *  Script what calculate the difference between actual date and 22 of february
 	 */
 
-	$fechaActual = date('Y-m-d',time()); //Get fecha actual en formato AAAA-MM-DD
-	$fechaActual = date_create($fechaActual); //Set fecha actual tipo DATE
-	$fechaFin = date_create('2018-02-22'); //Set fecha fin tipo DATE
- 	$dias; //Dias restantes
+	$fechaActual = date('Y-m-d',time()); //Get actual date (string in AAAA-MM-DD)
+	$fechaActual = date_create($fechaActual); //Set actual date (type DATE)
+	$fechaFin = date_create('2018-02-22'); //Set last day (type DATE)
+ 	$dias; //Days left
 	
-	$interval = date_diff($fechaActual, $fechaFin); //RESTAR DIAS
-	
+	$interval = date_diff($fechaActual, $fechaFin); //Difference between dates	
  	
  	$dias = $interval->d;
 
