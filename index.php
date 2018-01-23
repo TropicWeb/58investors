021d719f45ab0b3d04d894fa2015a0f656e07aa4
Diego Di Salvatore <disalvatorediego@gmail.com>
2018-01-23 19:25:54 -0400

    Fix zona horaria en index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   //To test the counter equals $actualDate to a date in AAAA-MM-DD in the line 8 and uncomment it. Comment line 11
   //$actualDate = '2018-02-15';
   //
+  date_default_timezone_set('America/Caracas');
 
   $actualDate = date('Y-m-d',time()); //Get actual date (string in AAAA-MM-DD)
   $actualDate = date_create($actualDate); //Set actual date (type DATE)
