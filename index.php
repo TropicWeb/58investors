7487bc4713a96aac23c981c5a2fd4e07ddab8285
Miguel PC <amomymoder@gmail.com>
2018-01-23 22:26:11 -0400

    Modificacion del maquetado y Responsive

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
    */
 
   //TEST
-  //To test the counter equals $actualDate to a date in AAAA-MM-DD in the line 8 and uncomment it. Comment line 11
+  //To test the counter equals $actualDate to a date in AAAA-MM-DD in the line 8 and uncomment it. Comment line 12
   //$actualDate = '2018-02-15';
   //
   date_default_timezone_set('America/Caracas');
@@ -25,40 +25,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta name="description" content="Landing Page de la productora +58Films">
-  <meta name="author" content="Miguel Fraga and Gabriel Taccone">
+  <meta name="author" content="Miguel Fraga ,Diego Di Salvatore and Gabriel Taccone">
 
   <title>+58INVESTORS</title>
 
   <link rel="stylesheet" href="CSS/bootstrap.min.css">
   <link rel="stylesheet" href="CSS/EstilosC30days.css">
   <link rel="stylesheet" href="CSS/animate.css">
-
-
 </head>
 
 <body>
 
+<!-- (Sección 1) Contenedor con el contador e imágen de fondo -->
 <section class="container-fluid cuadroTotal">
 	<div class="row sectionCont">
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
-			<div id="prueba" class="prueba animated">
-    			<p id="prueba2" class="text-center animated postext"><?php echo $days; ?></p>
+			<div id="p1" class="contador animated">
+    			<p id="p2" class="text-center animated postext"><?php echo $days; ?></p>
     			<p class="text-center dayC"> DAYS </p>
-    		</div>
-  		</div>		
-  		<div class="col-md-2"></div>
+    	</div>
+  	</div>		
+  	<div class="col-md-2"></div>
 	</div> 
 </section>
 
-
-
-
-  
   <script src="JS/jquery.js"></script>
   <script src="JS/bootstrap.min.js"></script>
   <script src="JS/jquery.easing.min.js"></script>
   <script src="JS/efectEntry.js"></script>
-
 </body>
 </html>
